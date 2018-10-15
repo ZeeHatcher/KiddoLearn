2fbc47f96bba369f3c32432bf87c03370c409f98
ZeeHatcher <zee.hatcher010@gmail.com>
2018-10-15 18:27:54 +0800

    changed stuff I forgot

--- kiddo_learn.py
+++ kiddo_learn.py
@@ -8,53 +8,90 @@ root.geometry("500x500")
 root["bg"] = "white"
 def_bg = root.cget("bg")
 
+container = tk.Frame(root, bg=def_bg, padx=100, pady=100)
+container.pack(fill="both", expand=True)
+
 # Login Menu
-frame_loginmenu = tk.Frame(root, bg=def_bg)
-frame_username = tk.Frame(frame_loginmenu, bg=def_bg)
-frame_password = tk.Frame(frame_loginmenu, bg=def_bg)
+lg = tk.Frame(container, bg=def_bg)
+lg_form_fr = tk.Frame(lg, bg=def_bg)
 
-label_title = tk.Label(frame_loginmenu, text="Kiddo Learn", fg="orange", bg=def_bg, font="Helvetica 25 bold")
-label_username = tk.Label(frame_username, bg=def_bg, text="Username: ")
-label_password = tk.Label(frame_password, bg=def_bg, text="Password:  ")
+lg_title_lb = tk.Label(lg, text="Kiddo Learn", fg="orange", bg=def_bg, font="Helvetica 25 bold")
+lg_username_lb = tk.Label(lg_form_fr, bg=def_bg, text="Username: ")
+lg_password_lb = tk.Label(lg_form_fr, bg=def_bg, text="Password:  ")
 
-entry_username = tk.Entry(frame_username)
-entry_password = tk.Entry(frame_password)
+lg_username_ent = tk.Entry(lg_form_fr)
+lg_password_ent = tk.Entry(lg_form_fr)
 
-button_login = tk.Button(frame_loginmenu, text="Login", command=lambda : login(entry_username, entry_password, frame_mainmenu))
-button_createaccount = tk.Button(frame_loginmenu, text="Create New Account", command="")
+lg_login_bt = tk.Button(lg, text="Login", command=lambda : login(lg_username_ent, lg_password_ent, main))
+lg_create_bt = tk.Button(lg, text="Create New Account", command=lambda : (lg.pack_forget(), display_crtacc()))
 
-frame_loginmenu.place(relx=0.5, rely=0.4, anchor="center")
+def display_lg() :
+    lg.pack(fill="both", expand=True)
 
-label_title.pack()
+    lg_title_lb.pack()
 
-frame_username.pack(pady=5)
-label_username.pack(side="left")
-entry_username.pack(side="right")
+    lg_form_fr.pack(pady=5)
 
-frame_password.pack(pady=5)
-label_password.pack(side="left")
-entry_password.pack(side="right")
+    lg_username_lb.grid(row=1, column=1)
+    lg_username_ent.grid(row=1, column=2)
 
-button_login.pack()
-button_createaccount.pack(pady=5)
+    lg_password_lb.grid(row=2, column=1)
+    lg_password_ent.grid(row=2, column=2)
+
+    lg_login_bt.pack()
+    lg_create_bt.pack(pady=5)
+
+# Create Account Menu
+crtacc = tk.Frame(container, bg=def_bg)
+crtacc_form_fr = tk.Frame(crtacc, bg=def_bg)
+
+crtacc_title_lb = tk.Label(crtacc, text="Create New Account", bg=def_bg, font="Helvetica 15 bold")
+crtacc_username_lb = tk.Label(crtacc_form_fr, bg=def_bg, text="Username: ")
+crtacc_password_lb = tk.Label(crtacc_form_fr, bg=def_bg, text="Password:  ")
+crtacc_confirm_lb = tk.Label(crtacc_form_fr, bg=def_bg, text="Confirm Password:  ")
+
+crtacc_username_ent = tk.Entry(crtacc_form_fr)
+crtacc_password_ent = tk.Entry(crtacc_form_fr)
+crtacc_confirm_ent = tk.Entry(crtacc_form_fr)
+
+crtacc_create_bt = tk.Button(crtacc, text="Confirm", command=lambda : (crtacc.pack_forget(), display_lg()))
+
+def display_crtacc() :
+    crtacc.pack(fill="both", expand=True)
+
+    crtacc_title_lb.pack(fill="both")
+
+    crtacc_form_fr.pack(pady=5)
+
+    crtacc_username_lb.grid(row=1, column=1, sticky="e")
+    crtacc_username_ent.grid(row=1, column=2, sticky="w")
+
+    crtacc_password_lb.grid(row=2, column=1, sticky="e")
+    crtacc_password_ent.grid(row=2, column=2, sticky="w")
+
+    crtacc_confirm_lb.grid(row=3, column=1, sticky="e")
+    crtacc_confirm_ent.grid(row=3, column=2, sticky="w")
+
+    crtacc_create_bt.pack(pady=5)
 
 # Main Menu
-frame_mainmenu = tk.Frame(root, bg=def_bg)
-frame_profiles = tk.Frame(frame_mainmenu, bg=def_bg)
-frame_info = tk.Frame(frame_mainmenu, bg=def_bg)
+main = tk.Frame(container, bg=def_bg)
+main_profiles_fr = tk.Frame(main, bg=def_bg)
+main_info_fr = tk.Frame(main, bg=def_bg)
 
-label_mainmenu = tk.Label(frame_mainmenu, bg=def_bg, text="Main Menu", font="Times 15 bold")
-label_profiles = tk.Label(frame_profiles, bg=def_bg, text="Profiles", font="Times 10 bold")
-label_info = tk.Label(frame_info, bg=def_bg, text="Information", font="Times 10 bold")
+main_title_lb = tk.Label(main, bg=def_bg, text="Main Menu", font="Times 15 bold")
+main_profiles_lb = tk.Label(main_profiles_fr, bg=def_bg, text="Profiles", font="Times 10 bold")
+main_info_lb = tk.Label(main_info_fr, bg=def_bg, text="Information", font="Times 10 bold")
 
-frame_mainmenu.place(relx=0.5, rely=0.5, anchor="center")
-label_mainmenu.pack()
-frame_profiles.pack()
-label_profiles.pack()
-frame_info.pack()
-label_info.pack()
-
-
+def display_main() :
+    main.place(relx=0.5, rely=0.5, anchor="center")
+    main_title_lb.pack()
+    main_profiles_fr.pack()
+    main_profiles_lb.pack()
+    main_info_fr.pack()
+    main_info_lb.pack()
 
 # mainloop
+display_lg()
+
 root.mainloop()
--- modules/login.py
+++ modules/login.py
@@ -14,22 +14,24 @@ def check_file(file_name) :
         with open(file_name, "r") as pull_file :
             f = eval(pull_file.read())
         return f
-      
+
 def show(frame) :
   frame.lift()
 
-def create_account(existing_account_dict) :
-    existing_account_dict["username"].append(create_username)
-    existing_account_dict["password"].append(create_password)
+def create_account(entry_username, entry_password) :
+    existing_account_dict = check_file("account.txt")
+
+    existing_account_dict["username"].append(entry_username)
+    existing_account_dict["password"].append(entry_password)
 
     with open("account.txt", "w") as out_file :
         out_file.write("{}".format(existing_account_dict))
 
-def login(input_username, input_password, frame) :
+def login(entry_username, entry_password, frame) :
     existing_account_dict = check_file("account.txt")
 
-    username = input_username.get()
-    password = input_password.get()
+    username = entry_username.get()
+    password = entry_password.get()
 
     if (username in existing_account_dict["username"]) :
       index = existing_account_dict["username"].index(username)
@@ -40,4 +42,3 @@ def login(input_username, input_password, frame) :
         print("Please enter a correct password")
     else :
       print("Username not found")
-        
